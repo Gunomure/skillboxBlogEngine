74e07907a776941ca44ebef8c124d5404de8f7ab
dtyunyaev <dtyunyaev94@mail.ru>
2021-07-04 02:17:00 +0300

    initializing commit

---
+++ src/test/java/com/skillbox/blogengine/BlogEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillbox.blogengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
