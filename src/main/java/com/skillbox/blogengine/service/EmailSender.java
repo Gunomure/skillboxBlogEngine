34e37c57d4c4dab3c9c1d2af3d408d9601558196
Gunomure <ziXWSphz7HaJMvhU>
2021-09-21 21:22:54 +0300

    send email with html type content

--- src/main/java/com/skillbox/blogengine/service/EmailSender.java
+++ src/main/java/com/skillbox/blogengine/service/EmailSender.java
@@ -1,15 +1,20 @@
 package com.skillbox.blogengine.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailSender {
+    private final static Logger LOGGER = LogManager.getLogger(EmailSender.class);
 
-    @Autowired
+    private static final String FROM = "noreply@skillboxBlog.com";
     private JavaMailSender emailSender;
 
     public EmailSender(JavaMailSender emailSender) {
@@ -17,11 +22,17 @@ public class EmailSender {
     }
 
     public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreply@skillboxBlog.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        emailSender.send(message);
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+
+        try {
+            helper.setText(text, true); //true for html type
+            helper.setTo(to);
+            helper.setSubject(subject); // TODO не понимаю, почему остается email из конфига
+            helper.setFrom(new InternetAddress(FROM));
+            emailSender.send(message);
+        } catch (MessagingException e) {
+            LOGGER.error("Got error while sending email to {}", to, e);
+        }
     }
 }
